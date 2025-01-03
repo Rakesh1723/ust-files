dd3bbba41ccd73fb45c0e35359c0bf8d5d406b30
Rakesh1723 <ageerurakesh@gmail.com>
2025-01-03 10:52:54 +0530

    updated

--- Cloud/trainee-app-with-data-jpa-cloud-sqlserver/src/main/java/com/ust/traineeapp/exception/RecordNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ust.traineeapp.exception;
-
-public class RecordNotFoundException extends RuntimeException{
-    public RecordNotFoundException(String message){
-        super(message);
-    }
-}
--- Cloud/trainee-app-with-data-jpa-cloud/src/main/java/com/ust/traineeapp/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ust.traineeapp.repository;
-
-import com.ust.traineeapp.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project,Integer> {
-}
